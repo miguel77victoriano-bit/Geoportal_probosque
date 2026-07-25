3c8223242d4af4510ace98a6d40596be12269ba3
miguel77victoriano-bit <miguel77victoriano@gmail.com>
2026-07-25 09:54:16 -0600

    Update maps.js

--- maps.js
+++ maps.js
@@ -6,14 +6,14 @@ var map = L.map("map").setView([23.322,-102.334], 5);
 var osm = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
 var satelite = L.tileLayer(
-"https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
 {
 attribution:'Tiles © Esri'
 }
 ).addTo(map);
 
 var topo = L.tileLayer(
-"https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png",
+'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png',
 {
 attribution:'© OpenTopoMap'
 }
@@ -23,17 +23,17 @@ attribution:'© OpenTopoMap'
 
 //var Preventivos_Brechas_Lineas = L.tileLayer.wms("http://192.168.1.66:8080/geoserver/Basededatos_regionII/wms?", {
   //  Layers: "Preventivos (Brechas_Lineas)",
-    //format : "image/png",
-    //transparent : true } ).addTo(map); 
+  //  format : "image/png",
+  //  transparent : true } ).addTo(map); 
 
 //var Preventivos_Quemas = L.tileLayer.wms("http://192.168.1.66:8080/geoserver/Basededatos_regionII/wms?", {
   //  Layers: "Preventivos (Quemas)",
-    //format : "image/png",
-    //transparent : true } ).addTo(map); 
+  //  format : "image/png",
+  //  transparent : true } ).addTo(map); 
 
-   // Prueba URL temporal
+    //Prueba URL temporal
 var Preventivos_Brechas_Lineas = L.tileLayer.wms("https://gathered-thousand-dated-constraint.trycloudflare.com/geoserver/Basededatos_regionII/wms?", {
-  Layers: "Preventivos (Brechas_Lineas)",
+    Layers: "Preventivos (Brechas_Lineas)",
     format : "image/png",
     transparent : true } ).addTo(map); 
 
@@ -52,6 +52,9 @@ var baseMaps = {
 };
 
 L.control.layers(baseMaps).addTo(map);
+//var wms1 = {"Preventivos_Brechas_Lineas": Preventivos_Brechas_Lineas};
+
+//var wms2 = {"Preventivos_Quemas": Preventivos_Quemas};
 
 var overlayMaps = {
     "Preventivos_Brechas_Lineas": Preventivos_Brechas_Lineas,
@@ -166,12 +169,3 @@ map.on('mousemove', function(e){
         "Lat: " + e.latlng.lat.toFixed(6) +
         "<br>Lon: " + e.latlng.lng.toFixed(6);
 });
-
-
-
-
-
-
-
-
-
